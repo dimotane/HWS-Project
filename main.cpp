81f001151ec2ea5f8e9698d3d1b9901fd41f23f0
seaweedjay <aloisiusjgrzybowski@gmail.com>
2022-11-06 16:29:23 -0600

    more stuff

--- main.cpp
+++ main.cpp
@@ -4,38 +4,42 @@ using namespace std;
 #include <sstream>
 #include <string>
 #include <array>
-#include "queueAndProcess.h"
+//#include "queueAndProcess.h"
 
 //this function reads the input file and parses it into processes
 int readFile(string filepath){
     ifstream InputFile(filepath); //read in the input file
-    //--------------placeholders for while loop------------------
-     std::array<std::string, 6> pieces{ "", "",
+     array<string, 6> pieces{ "", "",
                                        "", "" , "", ""};
     string inputline; //input string
-    // int pid;//P_ID
-    // int burst; //Burst
-    // int arrival;//arrival
-    // int priority;//priority
-    // int deadline;//deadline
-    // int IO;//io
-    
-    char tab = '/t';
-    //----------------------------------------------------------
-
-    if(getline(InputFile, inputline)){
-        getline(InputFile, inputline); //if there is anything in the file, we take that line.
-        //it's just the header so we don't need it
-    }
+    char tab = '\t';
+    int counter;
+   
+   getline(InputFile, inputline);//get rid of header line
    while( getline(InputFile, inputline)){ //for the rest of the lines in the file, we do stuff to it:
-        stringstream stream(inputline);
+        stringstream stream(inputline);//read in from current line
+         cout << "a line from your array: ";
+        counter = 0;
+        while(!stream.eof()){
+            getline(stream, pieces[counter], tab);
+            cout << pieces[counter]<< endl;
+            counter++;
+        } //this while loop parses 1 process into 6 strings
 
-        for(int i=0; i<6; i++){
-            if(getline(stream, pieces[i], tab)){
-                getline(stream, pieces[i], tab); //get the next thing
-            }
+        int pid = stoi(pieces[0]);
+        int burst = stoi(pieces[1]);
+        int arrival = stoi(pieces[2]);
+        int priority = stoi(pieces[3]);
+        int deadline = stoi(pieces[4]);
+        int io = stoi(pieces[5]); //parse strings as ints 
+
+        if(burst<1 || priority <0 || priority>99 || io > burst){
+            continue; //skip any processes that have weird input. no negative numbers, no deadlines before arrival, etc
+        }else{
+            Process process = new Process();
+            
         }
-    cout << "this is a line from your array: " << pieces[0];
+
    }
 
     return 0;
