91e1315145fd1bb0eb37797f1b21d0d14f4e8fd5
dimotane <dimotane@protonmail.com>
2022-11-20 02:39:28 -0600

    added a new header file with libraries and file read

---
+++ framework.h
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <array>
+#include <vector>
+#include "queueAndProcess.h"
+
+using namespace std;
+
+//this header file includes most of the libraries main uses, as well as the queue/process header file. 
+//it also has the file read function for convenience's sake
+
+//this function reads the input file and returns a vector of all the valid processes in it
+//alternitively, if hasFile is false, the user can input processes manually
+std::vector <Process> readFile(string filepath, bool hasFile){
+    vector <Process> pVector;
+    ifstream InputFile(filepath); //read in the input file
+    array <string, 6> pieces{ "","","","","",""};
+    string inputline; //input string
+    char tab = '\t';
+    int counter;
+    if (hasFile) { 
+        std::cout << "[-] Reading input file...";
+        getline(InputFile, inputline);//get rid of header line
+        getline(InputFile, inputline);//get the first line
+        do //using do while here because otherwise it skips the nextProcess line
+        {
+             //for the rest of the lines in the file, we do stuff to it:
+                stringstream stream(inputline);//read in from current line
+                counter = 0;
+                while(!stream.eof()) {
+                    getline(stream, pieces[counter], tab);
+                    counter++;
+                } //this while loop parses 1 process into 6 strings
+
+                int pid = stoi(pieces[0]);
+                int burst = stoi(pieces[1]);
+                int arrival = stoi(pieces[2]);
+                int priority = stoi(pieces[3]);
+                int deadline = stoi(pieces[4]);
+                int io = stoi(pieces[5]); //parse strings as ints 
+
+                if (burst<1 || burst > 100 || priority <0 || priority>99 || io > burst || arrival < 1 || pid < 1) {
+                    continue; //skip any processes that have weird input. no negative numbers, no deadlines before arrival, etc
+                } else
+                {   //create process with data gathered
+                    Process process = Process(pid, burst, priority, arrival, io);
+                    pVector.push_back(process); //add process to the end of the vector
+                } 
+        
+        } while(getline(InputFile, inputline));
+       //that's everything for the file
+        std::cout << "Done!" << endl;
+    }else
+    {
+        bool exit = false;
+        bool first = true;
+        string response;
+        while(!exit){
+            if (!first) {
+            std::cout << endl << "[?] Would you like to enter another process?" << endl << "[-] Type \"yes\" to enter a process, or type \"no\" to continue. ";
+            std::cin >> response;
+            }
+            if((response.compare("Yes") == 0 || response.compare("yes") == 0 || response.compare("YES") == 0) || first) {
+                //do the intake code
+                std::cout << "[-] Please enter process ID, burst, arrival, priority, deadline, and I/O separated by spaces: ";
+                std::cin >> inputline;
+                counter = 0;
+                istringstream iss(inputline); //read the input string
+                while(!iss.eof()){
+                getline(iss, pieces[counter]);
+                counter++;
+            } //this while loop parses 1 process into 6 strings
+
+            int pid = stoi(pieces[0]);
+            int burst = stoi(pieces[1]);
+            int arrival = stoi(pieces[2]);
+            int priority = stoi(pieces[3]);
+            int deadline = stoi(pieces[4]);
+            int io = stoi(pieces[5]); //parse strings as ints 
+
+            if (burst<1 || burst > 100 || priority <0 || priority>99 || io > burst || arrival < 1 || pid < 1 ){
+                std::cout << endl << "[X] This process is invalid."; //skip any processes that have weird input. no negative numbers, no deadlines before arrival, etc
+            } else {
+            Process process = Process(pid, burst, priority, arrival, io);
+            pVector.push_back(process); //add process to the end of the vector
+            }
+            } else if (response.compare("No") == 0 || response.compare("no") == 0 || response.compare("NO") == 0){
+                exit = true; //continue
+            } else {
+                std::cout << endl << "[-] Please only type \"yes\" or \"no\". ";
+            }
+        }
+    }
+    return pVector;
+}
